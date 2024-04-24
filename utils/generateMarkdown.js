32bfe5c80c7b37ee6ab20847f6a7ccbb71a66c86
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-24 16:47:41 -0400

    Added links to the badges if chosen by the user.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,13 +2,13 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)]`
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   } else if (license === 'APACHE 2.0') {
-    return `[![License: APACHE 2.0](https://img.shields.io/badge/License-APACHE_2.0-blue.svg)]`
+    return `[[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
   } else if (license === 'GPLv3.0') {
-    return `[![License: GPLv3.0](https://img.shields.io/badge/License-GPLv3.0-blue.svg)]`
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
   } else if (license === 'BSD_3') {
-    return `[![License: BSD_3](https://img.shields.io/badge/License-BSD_3-blue.svg)]`
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
   } else {
     return '';
   }
