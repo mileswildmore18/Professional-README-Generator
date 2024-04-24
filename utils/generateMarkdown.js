e428ee741e994a088c55d0c273c566baf50d1357
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-24 12:24:29 -0400

    Added table of contents in markdown and added spacing in between titles for the README.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,69 +12,67 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
 
-  return license ?`*[License](#license)` : ``;
+  return license ? `*[License](#license)` : ``;
 }
 
 // Creates a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { 
-if (license !== 'None'){
-  return `
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `
 
 ## License
 This project is licensed with ${license}`
+  }
+  return ``;
 }
-return ``;
-} 
 // Creates a function that generates markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.title}
   ${renderLicenseBadge(data.license)}
-  ## Description
+ ## Description
   
 ${data.description}
 
  ## Table of Contents
   * [Description](#description)
-
   * [Installation](#installation)
-
   * [Usage](#usage)
      ${renderLicenseLink(data.license)}
   * [Contributing](#contributing)
-
   * [Test](#tests)
+  * [Questions](#questions)
 
   
 
-  [Installation]
+  ## Installation
   
   To install the necessary dependencies, run the following command:
   \`\`\`
   ${data.installation}
   \`\`\`
 
-  [Usage]
-  
+  ## Usage
   ${data.usage}
+
   ${renderLicenseSection(data.license)}
-  [License]
+  ## License
   
   ${data.license}
 
-  [Contribution]
+  ## Contribution
   
   ${data.contributing}
 
-  [Tests]
+  ## Tests
   ${data.tests}
 
-  [Questions(#questions)]
+  ## Questions
 
   If you would like to contact me for further information, be sure to reach out to me at ${email}
   
-  If you would like to know how to setup dependencies, check out my reop at ${GitHub}
+  If you would like to know how to setup dependencies, check out my repo at ${GitHub}
   
   `;
 }
